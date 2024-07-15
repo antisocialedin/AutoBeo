175b7a0e44105ac320b7e952864ecf496a69167d
Eder Filho <ederlopes_7@hotmail.com>
2024-07-15 18:48:36 -0300

    add dhcp package install

--- 1_dependencias.py
+++ 1_dependencias.py
@@ -10,7 +10,8 @@ os.system("apt-get install net-tools")
 os.system("apt-get install ifupdown") 
 os.system("apt-get install openssh-server")
 os.system("apt-get install libopenmpi-dev")
-#os.system("apt-get install nfs-kernel-server") #apenas mestre
+os.system("apt-get install isc-dhcp-server") #apenas mestre
+os.system("apt-get install nfs-kernel-server") #apenas mestre
 os.system("apt-get install nfs-common")
 os.system("apt-get install portmap")
 os.system("apt-get install build-essential")
