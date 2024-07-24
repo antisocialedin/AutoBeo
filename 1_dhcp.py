32125fd2041ee6d907cc1146863bfc22d7795844
Eder Filho <ederlopes_7@hotmail.com>
2024-07-23 21:24:46 -0300

    Update 1_dhcp.py

--- 1_dhcp.py
+++ 1_dhcp.py
@@ -10,14 +10,14 @@ import os
 #criando rede
 os.system("sudo ifconfig enp0s3 192.168.40.1 netmask 255.255.255.0​")
 
-os.system("echo > /etc/default/isc-dhcp-server") #limpa o arquivo
+os.system(" > /etc/default/isc-dhcp-server") #limpa o arquivo
 
 arquivo = open("/etc/default/isc-dhcp-server​", "a")
 dhcp = list()
 dhcp.append('INTERFACESv4="enp0s3" \n') #verificar nome da placa
 arquivo.writelines(dhcp)
 
-os.system("echo > /etc/dhcp/dhcpd.conf") #limpa o arquivo
+os.system(" > /etc/dhcp/dhcpd.conf") #limpa o arquivo
 
 arquivo = open("/etc/dhcp/dhcpd.conf", "a")
 dhcp2 = list()
