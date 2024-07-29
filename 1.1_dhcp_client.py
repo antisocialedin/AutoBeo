b28b908aae89c6dd75b27988825f5d90f4a008ae
Eder Filho <ederlopes_7@hotmail.com>
2024-07-29 20:44:03 -0300

    Create 1.1_dhcp_client.py

---
+++ 1.1_dhcp_client.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# Eder Filho
+# 29/07/2023
+# SCRIPT PARA CONFIGURAÇÃO DE CLUSTER - DHCP CLIENT
+
+#import os
+import subprocess
+
+result = subprocess.run(['dhcp-lease-list'], capture_output=True, text=True)
+print(result.stdout)
