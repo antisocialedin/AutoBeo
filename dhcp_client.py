0736f9d98ed8792df1dd1d91e3f2f9829d76721c
Eder Filho <ederlopes_7@hotmail.com>
2024-08-15 21:06:37 -0300

    rename dhcp_client.py

--- dhcp_client.py
+++ dhcp_client.py
@@ -9,6 +9,9 @@ import subprocess
 # Executa o comando com shell=True para permitir encadeamento de comandos
 result = subprocess.run('dhcp-lease-list | tail -n +4 | cut -d " " -f 3', shell=True, capture_output=True, text=True)
 
-# Imprime a saída capturada
-print(result.stdout)
+# Divide a saída em linhas e armazena em uma lista
+ip_list = result.stdout.splitlines()
+
+# Imprime a lista de IPs
+print(ip_list)
 
