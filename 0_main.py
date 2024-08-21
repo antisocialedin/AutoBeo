d45cc71d5e83d0d66876aefb61cae3ccf06d2062
Eder Filho <ederlopes_7@hotmail.com>
2024-08-21 16:36:00 -0300

    Change and add new options

--- 0_main.py
+++ 0_main.py
@@ -10,10 +10,12 @@ while True:
     #Verificação de máquina
     print("---------------------- Script Cluster ----------------------\n")
     print("Digite 1 - Configurar servidor DHCP \n")
-    print("Digite 2 - Restart servidor DHCP \n")
-    print("Digite 3 - Configurar o servidor SSH \n") 
-    print("Digite 4 - Configurar o mestre \n") 
-    print("Digite 5 - Configurar os escravos \n")
+    print("Digite 2 - Start servidor DHCP \n")
+    print("Digite 3 - Status servidor DHCP \n")
+    print("Digite 4 - Restart servidor DHCP \n")
+    print("Digite 5 - Configurar o servidor SSH \n") 
+    print("Digite 6 - Configurar NFS (Network File System) \n") 
+    print("Digite 7 - Configurar os escravos \n")
     print("Digite 99 - SAIR \n")
 
     maquina = int(input("~: "))
@@ -24,17 +26,29 @@ while True:
             os.system("sudo python3 1_dhcp.py")
 
         case 2:
+            print("Start servidor DHCP...")
+            os.system("sudo service isc-dhcp-server start")
+        
+        case 3:
+            print("Status servidor DHCP...")
+            os.system("sudo service isc-dhcp-server status")
+
+        case 4:
             print("Restart servidor DHCP...")
             os.system("sudo service isc-dhcp-server restart")
 
-        case 3:
+        case 5:
             print("Configurando servidor ssh...")
             os.system("sudo python3 4_ssh.py")
 
-        case 4:
-            print("Configurando o mestre...")
+        case 6:
+            print("Configurando NFS mestre...")
+            os.system("sudo python3 5_nfs_mestre.py")
 
-        case 5:
+            print("Configurando NFS nos...")
+            os.system("sudo python3 5_nfs_nos.py")
+            
+        case 7:
             print("Configurando os escravos...")
 
         case 99:
