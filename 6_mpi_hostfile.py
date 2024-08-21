ef74c658f675a61b38539f21f75d9f77ffbe5f5c
Eder Filho <ederlopes_7@hotmail.com>
2024-08-21 17:07:19 -0300

    parameter mpi_hostfile

---
+++ 6_mpi_hostfile.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# Eder Filho
+# 09/05/2024
+# SCRIPT PARA CONFIGURAÇÃO DE CLUSTER - MPI
+
+import os
+
+os.system("echo > ~/.mpi_hostfile") #limpa o arquivo
+
+#editar hostfile (apenas Mestre)
+arquivo = open(".mpi_hostfile", "a")
+hostfile = list()
+hostfile.append("localhost slots= {Digite a quantidade de núcleos do mestre} \n")
+hostfile.append("{IP da Máquina 1} slots = {Digite a quantidade de núcleos} \n")
+hostfile.append("{IP da Máquina 2} slots= {Digite a quantidade de núcleos} \n")
+hostfile.append("{IP da Máquina n} slots= {Digite a quantidade de núcleos} \n")
+arquivo.writelines(hostfile)
