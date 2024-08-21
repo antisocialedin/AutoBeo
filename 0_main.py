288246a4488374af257475b5112a9a39097e3e3e
Eder Filho <ederlopes_7@hotmail.com>
2024-08-21 17:07:34 -0300

    Update 0_main.py

--- 0_main.py
+++ 0_main.py
@@ -9,18 +9,36 @@ while True:
 
     #Verificação de máquina
     print("---------------------- Script Cluster ----------------------\n")
+    print("Digite 0 - Instalação e Configuração Completa \n")
     print("Digite 1 - Configurar servidor DHCP \n")
     print("Digite 2 - Start servidor DHCP \n")
     print("Digite 3 - Status servidor DHCP \n")
     print("Digite 4 - Restart servidor DHCP \n")
     print("Digite 5 - Configurar o servidor SSH \n") 
     print("Digite 6 - Configurar NFS (Network File System) \n") 
-    print("Digite 7 - Configurar os escravos \n")
+    print("Digite 7 - Gerar .mpi_hostfile \n")
     print("Digite 99 - SAIR \n")
 
-    maquina = int(input("~: "))
+    opt = int(input("~: "))
 
-    match maquina:
+    match opt:
+
+        case 0:
+            print("Configurando servidor DHCP...")
+            os.system("sudo python3 1_dhcp.py")
+
+            print("Configurando servidor ssh...")
+            os.system("sudo python3 4_ssh.py")
+
+            print("Configurando NFS mestre...")
+            os.system("sudo python3 5_nfs_mestre.py")
+
+            print("Configurando NFS nos...")
+            os.system("sudo python3 5_nfs_nos.py")
+
+            print("Gerando .mpi_hostfile...")
+            os.system("sudo python3 6_mpi_hostfile.py")
+
         case 1:
             print("Configurando servidor DHCP...")
             os.system("sudo python3 1_dhcp.py")
@@ -47,9 +65,10 @@ while True:
 
             print("Configurando NFS nos...")
             os.system("sudo python3 5_nfs_nos.py")
-            
+
         case 7:
-            print("Configurando os escravos...")
+            print("Gerando .mpi_hostfile...")
+            os.system("sudo python3 6_mpi_hostfile.py")
 
         case 99:
             print("Saindo...")
