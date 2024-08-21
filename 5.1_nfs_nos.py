735a3101ce5dfd3de2dbe32235add5f184651c1b
Eder Filho <ederlopes_7@hotmail.com>
2024-08-21 16:38:46 -0300

    fix ip server dhcp

--- 5.1_nfs_nos.py
+++ 5.1_nfs_nos.py
@@ -11,14 +11,14 @@ for ip in ip_list:
 
     os.system(f"ssh cluster@{ip}")
     os.system("mkdir clusterdir")
-    os.system("sudo mount -t nfs 192.168.40.0:/home/cluster/clusterdir /home/cluster/clusterdir")
+    os.system("sudo mount -t nfs 192.168.40.1:/home/cluster/clusterdir /home/cluster/clusterdir")
 
     #editar fstab (apenas Nós)
     os.system("echo > /etc/fstab") #limpa o arquivo
-    
+
     arquivo = open("/etc/fstab", "a")
     fstab = list()
-    fstab.append("192.168.40.0:/home/cluster/clusterdir /home/cluster/clusterdir nfs rw,sync,hard,int 0 0 \n")
+    fstab.append("192.168.40.1:/home/cluster/clusterdir /home/cluster/clusterdir nfs rw,sync,hard,int 0 0 \n")
     arquivo.writelines(fstab)
 
     os.system("exit")
