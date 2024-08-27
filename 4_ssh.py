f54dcc917ede2bd44bf72122a422bf55fec3f045
Eder Filho <ederlopes_7@hotmail.com>
2024-08-26 22:23:28 -0300

    Update 4_ssh.py

--- 4_ssh.py
+++ 4_ssh.py
@@ -25,11 +25,7 @@ ssh_key_path = os.path.join(ssh_key_dir, "id_rsa")
 print(f"Gerando chave ssh em {ssh_key_path}...")
 os.system(f"ssh-keygen -f {ssh_key_path}")
 
-#caminho da pasta .ssh
-print("Caminhando para a pasta .ssh...")
-os.chdir("/home/cluster/.ssh")
-
 # Copia a chave para cada IP na lista
 print("Copiando chave para os IPs...")
 for ip in ip_list:
-    os.system(f"ssh-copy-id -i /home/cluster/.ssh/id_rsa.pub cluster@{ip}")
+    os.system(f"ssh-copy-id -i {ssh_key_path}.pub cluster@{ip}")
