2280819acbdba1006c7aaa29f94ec92780b51b53
Eder Filho <ederlopes_7@hotmail.com>
2024-09-01 21:33:02 -0300

    fix ssh connect

--- 5_nfs_nos.py
+++ 5_nfs_nos.py
@@ -4,10 +4,11 @@
 # SCRIPT PARA CONFIGURAÇÃO DE CLUSTER - NFS
 
 import os
+import paramiko # type: ignore
 from dhcp_get_ip import ip_list  # Importa a lista de IPs do arquivo ip_list.py
 
 #criar e copiar conteudo do diretório compartilhado (apenas Nós)
-for ip in ip_list:
+""" for ip in ip_list:
 
     os.system(f"ssh cluster@{ip}")
     os.system("mkdir ~/clusterdir")
@@ -21,4 +22,41 @@ for ip in ip_list:
     fstab.append("192.168.40.1:/home/cluster/clusterdir /home/cluster/clusterdir nfs rw,sync,hard,int 0 0 \n")
     arquivo.writelines(fstab)
 
-    os.system("exit")
+    os.system("exit") """
+
+for ip in ip_list:
+    # Configurações SSH
+    hostname = ip  # IP do nó1, por exemplo
+    username = 'cluster'
+    password = '1234'
+
+    # Cria o cliente SSH
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+    try:
+        # Conecta ao host
+        client.connect(hostname, username=username, password=password)
+
+        # Define o conjunto de comandos
+        comandos = """
+        mkdir ~/clusterdir &&
+        sudo mount -t nfs 192.168.40.1:/home/cluster/clusterdir /home/cluster/clusterdir &&
+        sudo sh -c 'echo "" > /etc/fstab' &&
+        echo "192.168.40.1:/home/cluster/clusterdir /home/cluster/clusterdir nfs rw,sync,hard,int 0 0" | sudo tee -a /etc/fstab
+        """
+
+        # Executa os comandos no nó remoto
+        stdin, stdout, stderr = client.exec_command(comandos)
+
+        # Lê a saída e os erros do comando
+        output = stdout.read().decode()
+        erros = stderr.read().decode()
+
+        # Exibe a saída e os erros
+        print("Saída:", output)
+        print("Erros:", erros)
+
+    finally:
+        # Fecha a conexão
+        client.close()
