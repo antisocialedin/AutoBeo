ae0de02d748ce8a1a06be990aa47b3df2484e0b7
Eder Filho <ederlopes_7@hotmail.com>
2024-09-02 20:32:45 -0300

    Update 5_nfs_nos.py

--- 5_nfs_nos.py
+++ 5_nfs_nos.py
@@ -9,7 +9,7 @@ from dhcp_get_ip import ip_list  # Importa a lista de IPs do arquivo ip_list.py
 for ip in ip_list:
     # Configurações SSH
     hostname = ip  # IP do nó, por exemplo
-    username = 'cluster'
+    username = 'root'
     password = '1234'
 
     # Cria o cliente SSH
