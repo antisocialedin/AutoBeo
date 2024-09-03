2af9cc4b8085cd91ab0409ad03cba90ba8f343a6
Eder Filho <ederlopes_7@hotmail.com>
2024-09-02 22:17:27 -0300

    Paramiko NFS

--- 5_nfs_nos.py
+++ 5_nfs_nos.py
@@ -3,21 +3,35 @@
 # 15/08/2024
 # SCRIPT PARA CONFIGURAÇÃO DE CLUSTER - NFS
 
-import os
+#!/usr/bin/env python
+# Eder Filho
+# 15/08/2024
+# SCRIPT PARA CONFIGURAÇÃO DE CLUSTER - NFS
+
+import paramiko
 from dhcp_get_ip import ip_list  # Importa a lista de IPs do arquivo ip_list.py
 
+def configure_node(ip):
+    # Conectar ao nó via SSH
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(ip, username='cluster')
+
+    # Criar diretório compartilhado
+    ssh.exec_command("mkdir -p /home/cluster/clusterdir")
+
+    # Limpar e editar o arquivo fstab
+    sftp = ssh.open_sftp()
+    with sftp.file('/etc/fstab', 'w') as fstab_file:
+        fstab_file.write("192.168.40.1:/home/cluster/clusterdir /home/cluster/clusterdir nfs rw,sync,hard,int 0 0\n")
+    
+    # Montar o diretório
+    ssh.exec_command("sudo mount -t nfs 192.168.40.1:/home/cluster/clusterdir /home/cluster/clusterdir")
+
+    # Fechar conexões
+    sftp.close()
+    ssh.close()
+
+# Configurar todos os nós
 for ip in ip_list:
-    #criar diretório compartilhado nos
-    os.system("ssh cluster@{ip}")
-               
-    os.system("mkdir /home/cluster/clusterdir")
-
-    #editar fstab (apenas Nós)
-    os.system(" > /etc/fstab") #limpa o arquivo
-
-    arquivo = open(" /etc/fstab", "a")
-    fstab = list()
-    fstab.append("192.168.40.1:/home/cluster/clusterdir /home/cluster/clusterdir nfs rw,sync,hard,int 0 0 \n")
-    arquivo.writelines(fstab)
-
-    os.system("sudo mount -t nfs 192.168.40.1:/home/cluster/clusterdir /home/cluster/clusterdir")
+    configure_node(ip)
